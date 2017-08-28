7cf18af8406c4e95eabd9239ab42624dc88a5301
gaarangoa <gaarangoa@gmail.com>
2017-08-28 11:57:01 -0400

    fix

--- run.py
+++ run.py
@@ -1088,7 +1088,7 @@ def get_assembly_logs():
 		if lid=="assembly":
 			l1=logassembly(rootvar.__ROOTPRO__+"/"+pid+"/assembly/idba_ud/"+sid+"/")
 		return jsonify(matrix=l1)
-	except exeption as error:
+	except Exception as error:
 		return jsonify({status: str(error)})
 
 
