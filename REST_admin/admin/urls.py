135fddc01508c79ee76fe937dea9a94ef6e5abbe
gaarangoa <gaarangoa@gmail.com>
2018-09-05 20:48:46 -0400

    fix

--- REST_admin/admin/urls.py
+++ REST_admin/admin/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('metastorm_admin/admin/', admin.site.urls),
+    path('metastorm_admin/', admin.site.urls),
 ]
