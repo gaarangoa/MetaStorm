7335e863fc9f03698e7d4415c66dd3c42fcc9803
gaarangoa <gaarangoa@gmail.com>
2018-09-06 11:32:19 -0400

    fix

--- run.py
+++ run.py
@@ -1349,7 +1349,7 @@ def TaxonomyHTML():
     return render_template('TaxonomyTree.html')
 
 
-from app.lib.retrieve import get_results
+from app.lib.retrieve.retrieve import get_results
 
 
 @app.route('/status', methods=['POST'])
