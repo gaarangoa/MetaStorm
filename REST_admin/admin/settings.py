3a080a1605bed68ae38f33493eac57402b18f35e
gaarangoa <gaarangoa@gmail.com>
2018-09-05 20:43:31 -0400

    hosts

--- REST_admin/admin/settings.py
+++ REST_admin/admin/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'bench.cs.vt.edu'
+]
 
 
 # Application definition
