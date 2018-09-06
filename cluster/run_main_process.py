1ead1614354fa45a66eee9c30fa872553136c769
gaarangoa <gaarangoa@gmail.com>
2018-09-06 13:01:21 -0400

    fix

--- cluster/run_main_process.py
+++ cluster/run_main_process.py
@@ -60,7 +60,7 @@ try:
     main_run(data, refs, sid, uid, pip, dbfile, USER[0], SAMPLE[0])
     # make a request to the listener in the localhost
     os.system('ssh newriver1.arc.vt.edu python ' +
-              rootdir+'/listener.py '+sys.argv[1]+' completed')
+              rootdir+'/listener.py '+sys.argv[1]+' done')
 except Exception as inst:
     os.system('ssh newriver1.arc.vt.edu python ' +
               rootdir+'/listener.py '+sys.argv[1]+' failed')
