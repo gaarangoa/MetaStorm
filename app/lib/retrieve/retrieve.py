4dd207ad3c3e6cd6507f380e49ddbe147e81ca12
gaarangoa <gaarangoa@gmail.com>
2018-09-06 12:01:49 -0400

    fix

--- app/lib/retrieve/retrieve.py
+++ app/lib/retrieve/retrieve.py
@@ -115,7 +115,15 @@ def get_results(job='', status=''):
                 'Gustavo Arango', 'gustavo1@vt.edu', 'MetaStorm', messe.strip())
             messe2 = "Dear " + \
                 USER[0]['user_name'] + \
-                ",<br> During the execution of one of your samples an exception has been triggered and the job has been terminated. <br> A notification of the error has been sent to the Administrators of MetaStorm. Please check your submission parameters (databases, fastq files, etc.) and try it again. If the error persists, the MetaStorm team will contact you for further assistance. If you need inmediate assistance contact us at cmetangen@gmail.com. <br><br>This notification tool has been enabled in order to improve the efficience of MetaStorm. <br><br> Thanks, <br> MetaStorm Team"
+                ",<br> During the execution of one of your samples an exception has been triggered and " + \
+                " the job has been terminated. <br> A notification of the error has been sent to the " + \
+                " Administrators of MetaStorm. Please check your submission parameters (databases, fastq files, etc.) " + \
+                " and try it again. Please contact us at cmetangen@gmail.com providing the following information:  \n" + \
+                " Sample ID:" + sid + \
+                " Project ID:" + pid + \
+                " Pipeline: " + pip + \
+                "<br><br>This notification tool has been enabled in order to improve the efficience of MetaStorm. " + \
+                "<br><br> Thanks, <br> MetaStorm Team"
             x = email.send_email(
                 USER[0]['user_name'],
                 USER[0]['user_affiliation'],
