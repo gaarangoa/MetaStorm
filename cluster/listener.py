cb6a530065011a41cb526b8624d38c5931baa13e
gaarangoa <gaarangoa@gmail.com>
2018-09-06 12:16:10 -0400

    fix

--- cluster/listener.py
+++ cluster/listener.py
@@ -3,13 +3,10 @@ import json
 import base64
 import os
 
-inp = json.loads(base64.b64decode(sys.argv[1]))
-
-
 os.system(" ".join([
     ' curl ',
     ' --header "Content-Type: application/json"'
     ' --request POST',
-    ' --data '+json.dumps({"job": sys.argv[1], "status":sys.arg[2]}),
+    ' --data '+json.dumps({"job": sys.argv[1], "status": sys.argv[2]}),
     ' http://bench.cs.vt.edu/MetaStorm/status'
 ]))
