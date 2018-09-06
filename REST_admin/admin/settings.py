547d4ef2b591272a57c20eba58260ca70f05b250
gaarangoa <gaarangoa@gmail.com>
2018-09-05 20:30:22 -0400

    added django admin

--- REST_admin/admin/settings.py
+++ REST_admin/admin/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     # },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'database/metastorm.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database/projects.db'),
     }
 }
 
