279fdf080e4bfc2656acbac2d1c624862ae5e2d8
gaarangoa <gaarangoa@gmail.com>
2018-09-06 12:25:51 -0400

    fix

--- cluster/listener.py
+++ cluster/listener.py
@@ -6,6 +6,7 @@ os.system(" ".join([
     ' curl ',
     ' --header "Content-Type: application/json"'
     ' --request POST',
+    ' --max-time 5',
     " --data '"+json.dumps({"job": sys.argv[1], "status": sys.argv[2]})+"'",
     ' http://bench.cs.vt.edu/MetaStorm/status'
 ]))
