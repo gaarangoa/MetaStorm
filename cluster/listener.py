fc474734fc06a1d250a33438ea7e2b3c1ee9673c
gustavo arango <gaarangoa@gmail.com>
2019-04-24 13:57:51 -0400

    added new ms site

--- cluster/listener.py
+++ cluster/listener.py
@@ -2,11 +2,34 @@ import sys
 import json
 import os
 
-os.system(" ".join([
-    ' curl ',
-    ' --header "Content-Type: application/json"'
-    ' --request POST',
-    ' --max-time 5',
-    " --data '"+json.dumps({"job": sys.argv[1], "status": sys.argv[2], "message": sys.argv[3]})+"'",
-    ' http://bench.cs.vt.edu/MetaStorm/status'
-]))
+os.system(
+    " ".join(
+        [
+            " curl ",
+            ' --header "Content-Type: application/json"' " --request POST",
+            " --max-time 5",
+            " --data '"
+            + json.dumps(
+                {"job": sys.argv[1], "status": sys.argv[2], "message": sys.argv[3]}
+            )
+            + "'",
+            " http://bench.cs.vt.edu/MetaStorm/status",
+        ]
+    )
+)
+
+os.system(
+    " ".join(
+        [
+            " curl ",
+            ' --header "Content-Type: application/json"' " --request POST",
+            " --max-time 5",
+            " --data '"
+            + json.dumps(
+                {"job": sys.argv[1], "status": sys.argv[2], "message": sys.argv[3]}
+            )
+            + "'",
+            " http://bench.cs.vt.edu/metastorm2/status",
+        ]
+    )
+)
