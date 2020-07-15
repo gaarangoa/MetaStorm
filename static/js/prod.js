f35780be7114826448453a6851cf5c0a4f1a9672
Gustavo Arango <gustavo1@Gustavos-MacBook-Pro.local>
2020-07-14 22:12:40 -0400

    fix plupload directory on prod.js

--- static/js/prod.js
+++ static/js/prod.js
@@ -2,5 +2,6 @@ var Parameters = {
     // host: '/metastorm2/',
     host: '/',
     upload_dir: "/src/Files/PROJECTS/",
-    arc_pro: '/groups/metastorm_cscee/MetaStorm/Files/PROJECTS/'
+    arc_pro: '/groups/metastorm_cscee/MetaStorm/Files/PROJECTS/',
+    plupload: "http://localhost:17001/upload.php"
 }
